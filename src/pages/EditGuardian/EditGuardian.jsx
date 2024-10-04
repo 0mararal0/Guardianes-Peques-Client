0b9a0fb604d2b9e6381aae00bc4e334a2cea27ee
alberto marcos <amarcosarranz@gmail.com>
2024-10-04 02:01:20 +0200

    css

--- src/pages/EditGuardian/EditGuardian.jsx
+++ src/pages/EditGuardian/EditGuardian.jsx
@@ -1,3 +1,4 @@
+import "./styleEditGuardian.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
@@ -78,143 +79,152 @@ export const EditGuardian = () => {
 
   return (
     <>
-      <div className="d-flex flex-column justify-content-center align-items-center">
-        <Container className="m-5 text-center">
-          <Row>
-            <Col xs={12} md={12}>
-              <Image src={dataGuardian?.foto} roundedCircle />
-            </Col>
-          </Row>
-        </Container>
-        <Form noValidate validated={validated} onSubmit={handleSubmit}>
-          <Row className="mb-3">
-            <Form.Group as={Col} md="4" controlId="validationCustom01">
-              <Form.Label>Nombre</Form.Label>
-              <Form.Control
-                required
-                type="text"
-                placeholder="Nombre"
-                defaultValue={dataGuardian?.nombre}
-                onChange={handleName}
-              />
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="4" controlId="validationCustom02">
-              <Form.Label>Apellidos</Form.Label>
-              <Form.Control
-                required
-                type="text"
-                placeholder="Apellidos"
-                defaultValue={dataGuardian?.apellidos}
-                onChange={handleLastName}
-              />
-              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-              <Form.Label>Email</Form.Label>
-              <InputGroup hasValidation>
-                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+      <div className="d-flex flex-column justify-content-center align-items-center containerEditGuardian">
+        <h3 className="titleEditGuardian">Editar Guardian</h3>
+        <div className="containerFormEdirGuardian">
+          <Container className="my-5 text-center">
+            <Row>
+              <Col xs={12} md={12}>
+                <Image
+                  style={{ width: "200px" }}
+                  src={dataGuardian?.foto}
+                  roundedCircle
+                />
+              </Col>
+            </Row>
+          </Container>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Row className="mb-3">
+              <Form.Group as={Col} md="4" controlId="validationCustom01">
+                <Form.Label>Nombre</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="Nombre"
+                  defaultValue={dataGuardian?.nombre}
+                  onChange={handleName}
+                />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group as={Col} md="4" controlId="validationCustom02">
+                <Form.Label>Apellidos</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="Apellidos"
+                  defaultValue={dataGuardian?.apellidos}
+                  onChange={handleLastName}
+                />
+                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+                <Form.Label>Email</Form.Label>
+                <InputGroup hasValidation>
+                  <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                  <Form.Control
+                    type="text"
+                    placeholder="email"
+                    aria-describedby="inputGroupPrepend"
+                    required
+                    defaultValue={dataGuardian?.email}
+                    onChange={handleEmail}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    Please choose a username.
+                  </Form.Control.Feedback>
+                </InputGroup>
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+              <Form.Group as={Col} md="6" controlId="validationCustom03">
+                <Form.Label>Localidad</Form.Label>
                 <Form.Control
                   type="text"
-                  placeholder="email"
-                  aria-describedby="inputGroupPrepend"
+                  placeholder="Localidad"
                   required
-                  defaultValue={dataGuardian?.email}
-                  onChange={handleEmail}
+                  defaultValue={dataGuardian?.localidad}
+                  onChange={handleCity}
                 />
                 <Form.Control.Feedback type="invalid">
-                  Please choose a username.
+                  Please provide a valid city.
                 </Form.Control.Feedback>
-              </InputGroup>
-            </Form.Group>
-          </Row>
-          <Row className="mb-3">
-            <Form.Group as={Col} md="6" controlId="validationCustom03">
-              <Form.Label>Localidad</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Localidad"
-                required
-                defaultValue={dataGuardian?.localidad}
-                onChange={handleCity}
-              />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid city.
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="3" controlId="validationCustom04">
-              <Form.Label>Provincia</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Provincia"
-                required
-                defaultValue={dataGuardian?.provincia}
-                onChange={handleCountry}
-              />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid state.
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="3" controlId="validationCustom05">
-              <Form.Label>Teléfono</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Teléfono"
-                required
-                defaultValue={dataGuardian?.telefono}
-                onChange={handlePhone}
-              />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid zip.
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Row>
-          <Row className="mb-3">
-            <Form.Group as={Col} md="4" controlId="validationCustom06">
-              <Form.Label>Foto</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Foto"
-                required
-                defaultValue={dataGuardian?.foto}
-                onChange={handlePhoto}
-              />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid city.
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="2" controlId="validationCustom07">
-              <Form.Label>Tarifa</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Tarifa"
-                required
-                defaultValue={dataGuardian?.tarifa}
-                onChange={handleTarifa}
-              />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid state.
-              </Form.Control.Feedback>
-            </Form.Group>
-            <Form.Group as={Col} md="6" controlId="validationCustom08">
-              <Form.Label>Comentario</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Comentario"
-                required
-                defaultValue={dataGuardian?.comentario}
-                onChange={handleComentario}
-              />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid zip.
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Row>
-          <Button type="submit">Guardar</Button>
-          <Button type="button" onClick={() => navigate("/guardians")}>
-            Volver
-          </Button>
-        </Form>
+              </Form.Group>
+              <Form.Group as={Col} md="3" controlId="validationCustom04">
+                <Form.Label>Provincia</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Provincia"
+                  required
+                  defaultValue={dataGuardian?.provincia}
+                  onChange={handleCountry}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid state.
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group as={Col} md="3" controlId="validationCustom05">
+                <Form.Label>Teléfono</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Teléfono"
+                  required
+                  defaultValue={dataGuardian?.telefono}
+                  onChange={handlePhone}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid zip.
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+              <Form.Group as={Col} md="4" controlId="validationCustom06">
+                <Form.Label>Foto</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Foto"
+                  required
+                  defaultValue={dataGuardian?.foto}
+                  onChange={handlePhoto}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid city.
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group as={Col} md="2" controlId="validationCustom07">
+                <Form.Label>Tarifa</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Tarifa"
+                  required
+                  defaultValue={dataGuardian?.tarifa}
+                  onChange={handleTarifa}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid state.
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group as={Col} md="6" controlId="validationCustom08">
+                <Form.Label>Comentario</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Comentario"
+                  required
+                  defaultValue={dataGuardian?.comentario}
+                  onChange={handleComentario}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid zip.
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Row>
+            <div className="d-flex gap-4 justify-content-center m-4 mt-5  ">
+              <Button type="submit">Guardar</Button>
+              <Button type="button" onClick={() => navigate("/guardians")}>
+                Volver
+              </Button>
+            </div>
+          </Form>
+        </div>
       </div>
     </>
   );
--- src/pages/Reservation/Reservation.jsx
+++ src/pages/Reservation/Reservation.jsx
@@ -1,3 +1,4 @@
+import "./styleReservation.css";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Button, Card } from "react-bootstrap";
@@ -31,87 +32,88 @@ export const Reservation = () => {
 
   return (
     <>
-      <div>
-        <h2>Reservas</h2>
+      <div className="containerReservation">
+        <h3 className="titleReservation">Reservas</h3>
         {reservas?.map((guardian) => {
           return (
             <div key={guardian.id}>
-              <h3>
+              <h3 className="titleReservation2">
                 {guardian.client.length !== 0 && (
                   <p>Guardian {guardian.nombre}</p>
                 )}
               </h3>
-              {guardian.client.length !== 0 &&
-                guardian.client.map((res) => {
-                  return (
-                    <div key={res.id}>
-                      <Card>
-                        <Card.Header as="h5"></Card.Header>
-                        <Card.Body>
-                          <Card.Title>
-                            {res.nombre} {res.apellidos} {res.telefono}
-                          </Card.Title>
-                          <Card.Text>
-                            {res.direcion} {res.ciudad}
-                            {res.poblacion}
-                          </Card.Text>
-                          <Card.Text>
-                            {res.niños === "1" && "Un niño"}
-                            {res.niños === "2" && "Dos niños"}
-                            {res.niños === "3 niños" && "Tres"}
-                            {res.niños === "4" && "Cuatro o mas niños"} con edad{" "}
-                            {res.edadNiño.bebe && "- bebé"}
-                            {res.edadNiño.pequeño && "- pequeño"}
-                            {res.edadNiño.prescolar && "- prescolar"}
-                            {res.edadNiño.escolar && "- escolar"}
-                            {res.edadNiño.adolescente && "- adolescente"}
-                          </Card.Text>
-                          <Card.Text>
-                            Día de la reserva {res.reservationDay[0]} /
-                            {res.reservationDay[1]} /{res.reservationDay[2]}, a
-                            las {res.reservationHour[0]}:
-                            {res.reservationHour[1]}
-                          </Card.Text>
-                          <div className="row">
-                            {res.tareas.cocinar && (
-                              <Card.Text className="col-2">COCINAR</Card.Text>
-                            )}
-                            {res.tareas.actividades && (
-                              <Card.Text className="col-2">
-                                ACTIVIDADES
-                              </Card.Text>
-                            )}
-                            {res.tareas.baño && (
-                              <Card.Text className="col-2">BAÑO</Card.Text>
-                            )}
-                            {res.tareas.deberes && (
-                              <Card.Text className="col-2">DEBERES</Card.Text>
-                            )}
-                            {res.tareas.dormir && (
-                              <Card.Text className="col-2">DORMIR</Card.Text>
-                            )}
-                            {res.tareas.recogida && (
-                              <Card.Text className="col-2">RECOGIDA</Card.Text>
-                            )}
-                            {res.tareas.tareas && (
-                              <Card.Text className="col-2">
-                                TAREAS-CASA
-                              </Card.Text>
-                            )}
-                          </div>
-                          <Card.Text>Precio {guardian.tarifa}</Card.Text>
-
-                          <Button
-                            variant="primary"
-                            onClick={() => handleDeleted(res.id)}
-                          >
-                            Eliminar
-                          </Button>
-                        </Card.Body>
-                      </Card>
-                    </div>
-                  );
-                })}
+              <div className="d-flex">
+                {guardian.client.length !== 0 &&
+                  guardian.client.map((res) => {
+                    return (
+                      <div key={res.id}>
+                        <Card>
+                          <Card.Body>
+                            <Card.Title>
+                              {res.nombre} {res.apellidos} {res.telefono}
+                            </Card.Title>
+                            <Card.Text>
+                              {res.direcion} {res.ciudad} {res.poblacion}
+                            </Card.Text>
+                            <Card.Text>
+                              {res.niños === "1" && "Un niño"}
+                              {res.niños === "2" && "Dos niños"}
+                              {res.niños === "3 niños" && "Tres"}
+                              {res.niños === "4" && "Cuatro o mas niños"} con
+                              edad {res.edadNiño.bebe && "- bebé"}
+                              {res.edadNiño.pequeño && "- pequeño"}
+                              {res.edadNiño.prescolar && "- prescolar"}
+                              {res.edadNiño.escolar && "- escolar"}
+                              {res.edadNiño.adolescente && "- adolescente"}
+                            </Card.Text>
+                            <Card.Text>
+                              Día de la reserva {res.reservationDay[0]} /
+                              {res.reservationDay[1]} /{res.reservationDay[2]},
+                              a las {res.reservationHour[0]}:
+                              {res.reservationHour[1]}
+                            </Card.Text>
+                            <div className="row">
+                              {res.tareas.cocinar && (
+                                <Card.Text className="col-6">COCINAR</Card.Text>
+                              )}
+                              {res.tareas.actividades && (
+                                <Card.Text className="col-6">
+                                  ACTIVIDADES
+                                </Card.Text>
+                              )}
+                              {res.tareas.baño && (
+                                <Card.Text className="col-6">BAÑO</Card.Text>
+                              )}
+                              {res.tareas.deberes && (
+                                <Card.Text className="col-6">DEBERES</Card.Text>
+                              )}
+                              {res.tareas.dormir && (
+                                <Card.Text className="col-6">DORMIR</Card.Text>
+                              )}
+                              {res.tareas.recogida && (
+                                <Card.Text className="col-6">
+                                  RECOGIDA
+                                </Card.Text>
+                              )}
+                              {res.tareas.tareas && (
+                                <Card.Text className="col-6">
+                                  TAREAS-CASA
+                                </Card.Text>
+                              )}
+                            </div>
+                            <Card.Text>Precio {guardian.tarifa}</Card.Text>
+                            <Button
+                              variant="primary"
+                              onClick={() => handleDeleted(res.id)}
+                            >
+                              Eliminar
+                            </Button>
+                          </Card.Body>
+                        </Card>
+                      </div>
+                    );
+                  })}
+              </div>
             </div>
           );
         })}
